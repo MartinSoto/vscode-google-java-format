0cf34b43c24e9b38ecc5150ded4ee56a406dae9e
Martin Soto <donsoto@gmail.com>
2020-03-28 21:02:41 +0000

    Configure Prettier and apply it to all files

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,20 @@
 import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
-  console.log('Activating extension "google-java-format"');
+    console.log('Activating extension "google-java-format"');
 
-  vscode.languages.registerDocumentFormattingEditProvider("foo-lang", {
-    provideDocumentFormattingEdits(
-      document: vscode.TextDocument
-    ): vscode.TextEdit[] {
-      const firstLine = document.lineAt(0);
-      if (firstLine.text !== "42") {
-        return [vscode.TextEdit.insert(firstLine.range.start, "42\n")];
-      }
+    vscode.languages.registerDocumentFormattingEditProvider("foo-lang", {
+        provideDocumentFormattingEdits(
+            document: vscode.TextDocument
+        ): vscode.TextEdit[] {
+            const firstLine = document.lineAt(0);
+            if (firstLine.text !== "42") {
+                return [vscode.TextEdit.insert(firstLine.range.start, "42\n")];
+            }
 
-      return [];
-    }
-  });
+            return [];
+        },
+    });
 }
 
 // this method is called when your extension is deactivated
