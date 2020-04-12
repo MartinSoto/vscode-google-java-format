ce8204a86080f47fc2a19873ba42c033cb6d9a21
Martin Soto <donsoto@gmail.com>
2020-04-12 21:40:01 +0000

    Unit test now really runs
    ...thanks to chai-as-promised.

--- src/test/subproc.test.ts
+++ src/test/subproc.test.ts
@@ -1,12 +1,14 @@
-import * as assert from "assert";
+import * as chai from "chai";
+import * as chaiAsPromised from "chai-as-promised";
 import * as sut from "../subproc";
+
+chai.use(chaiAsPromised);
+chai.should();
 
 describe("subproc", function () {
     describe("#runProcess()", function () {
         it("should return Yeah!", function () {
-            sut.runProcess("ze input").then((output) => {
-                assert.equal(output, "yeah!");
-            });
+            return sut.runProcess("ze input").should.eventually.equal("Yeah!");
         });
     });
 });
